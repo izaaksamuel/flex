e62fb5b7b5ee9cf6b126a56e928b71c814bee0e7
izaaksamuel <148281432+izaaksamuel@users.noreply.github.com>
2024-09-04 21:44:52 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+let etanol, gasolina
+
+function calcular() {
+    etanol = frmFlex.inputEtanol.value
+    gasolina = frmFlex.inputGasolina.value
+    if(etanol <0.7 * gasolina){
+    document.getElementById('status').src= "img/etanol.png"
+}else {
+    document.getElementById('status').src= "img/gasolina.png"
+}
+return false //não submeter os dados do formulário
+
+    
+}
